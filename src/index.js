767f137a62afab853bc3963c6a3ff092787d5f72
MannanShariff <mannanshariff04@gmail.com>
2025-09-06 20:01:37 +0530

    Added files in the utils for custom api error and response handling and asyncHandler and made some changes in app.js and index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,48 @@ dotenv.config({
     path: './.env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+});
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+// Method 1 using promises
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+    }
+}
+
+export { asyncHandler };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// }
